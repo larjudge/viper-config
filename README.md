ec5c88406daef54be4342e87d5eefbaa1630c672
Lar Judge <laurence.judge@anz.com>
2020-03-04 16:53:38 +1100

    back to the drawing board

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-`go run main.go --intConfig=1 --stringConfig="hello world" --boolConfig=true`
+# Viper-Config
+
+Viper-Config is a really simple demo application that uses viper to parse both config files and command line flags
+
+Running with a flag: 
+```zsh
+go run main.go --intConfig=1 --stringConfig="hello world" --boolConfig=true
+```
+
+Running with config file:
+```zsh
+go run main.go --config "$PWD/config/local.yaml"
+```
+
+Running with both:
+```zsh
+go run main.go --config "$PWD/config/local.yaml" --stringConfig="yaml and flags"
+```
--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"github.com/mitchellh/mapstructure"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"io/ioutil"
 )
 
 var (
-	boolConfig   bool
-	intConfig    int
-	stringConfig string
+	configFile = flag.StringP("config", "c", "", "specific config file to use")
 
-	configFile = flag.String("config", "", "specific config file to use")
+	boolConfig   = flag.BoolP("boolConfig", "b", false, "some bool")
+	intConfig    = flag.IntP("intConfig", "i", 0, "some int")
+	stringConfig = flag.StringP("stringConfig", "s", "", "some string")
+
+	flagConfig = &Config{
+		AppSpec: AppSpec{
+			BoolConfig:   *boolConfig,
+			IntConfig:    *intConfig,
+			StringConfig: *stringConfig,
+		},
+	}
 )
 
 type Config struct {
@@ -48,6 +57,7 @@ func loadConfig(srcFile string) (*Config, error) {
 	var config Config
 
 	// convert from struct to generic map (required for viper to merge correctly) and set the defaults (will be used if not explicitly set via environment or config file)
+	fmt.Printf("\n\nDefault CONFIG IS %+v\n\n", defaultConfig)
 	viper.SetDefault("spec", config.GetMap(defaultConfig.AppSpec))
 
 	err := viper.ReadInConfig()
@@ -139,7 +149,13 @@ func (c Config) GetMap(config interface{}) map[string]interface{} {
 func main() {
 	flag.Parse()
 
-	config, _ := loadConfig(*configFile)
+	x, _ := ioutil.ReadFile(*configFile)
+	fmt.Println(string(x))
 
+
+	fmt.Printf("\n\nFlagCnfig IS %+v\n\n", flagConfig)
+
+	config, _ := loadConfig(*configFile)
+	fmt.Printf("\n\nFLAGS are \n\t%+v\n\t%+v\n\t%+v\n", *stringConfig, *intConfig, *boolConfig)
 	fmt.Printf("\n\nCONFIG IS %+v\n\n", config)
 }
