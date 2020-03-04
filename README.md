d7d6dc59d125aeafb384ca755d301323f92f7bc0
Lar Judge <laurence.judge@anz.com>
2020-03-04 17:33:04 +1100

    fully working

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Viper-Config is a really simple demo application that uses viper to parse both config files and command line flags
 
-Running with a flag: 
+Running flags: 
 ```zsh
-go run main.go --intConfig=1 --stringConfig="hello world" --boolConfig=true
+go run main.go --spec.intConfig=1 --spec.stringConfig="hello world" --spec.boolConfig=true
 ```
 
 Running with config file:
@@ -14,5 +14,5 @@ go run main.go --config "$PWD/config/local.yaml"
 
 Running with both:
 ```zsh
-go run main.go --config "$PWD/config/local.yaml" --stringConfig="yaml and flags"
+go run main.go --config "$PWD/config/local.yaml" --spec.stringConfig="yaml and flags"
 ```
--- main.go
+++ main.go
@@ -5,27 +5,17 @@ import (
 	"github.com/mitchellh/mapstructure"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"io/ioutil"
 )
 
 var (
 	configFile = flag.StringP("config", "c", "", "specific config file to use")
-
-	boolConfig   = flag.BoolP("boolConfig", "b", false, "some bool")
-	intConfig    = flag.IntP("intConfig", "i", 0, "some int")
-	stringConfig = flag.StringP("stringConfig", "s", "DEFAULT", "some string")
-
-	flagConfig = &Config{
-		AppSpec: AppSpec{
-			BoolConfig:   *boolConfig,
-			IntConfig:    *intConfig,
-			StringConfig: *stringConfig,
-		},
-	}
+	boolConfig   = flag.BoolP("spec.boolConfig", "b", false, "some bool")
+	intConfig    = flag.IntP("spec.intConfig", "i", 0, "some int")
+	stringConfig = flag.StringP("spec.stringConfig", "s", "DEFAULT", "some string")
 )
 
 type Config struct {
-	AppSpec AppSpec `json:"spec"           yaml:"spec"          mapstructure:"spec"`
+	AppSpec AppSpec `json:"spec" yaml:"spec" mapstructure:"spec"`
 }
 
 type AppSpec struct {
@@ -37,9 +27,9 @@ type AppSpec struct {
 var (
 	defaultConfig = &Config{
 		AppSpec: AppSpec{
-			BoolConfig:   false,
-			IntConfig:    0,
-			StringConfig: "",
+			BoolConfig:   *boolConfig,
+			IntConfig:    *intConfig,
+			StringConfig: *stringConfig,
 		},
 	}
 )
@@ -59,7 +49,6 @@ func loadConfig(srcFile string, fl flag.FlagSet) (*Config, error) {
 	var config Config
 
 	// convert from struct to generic map (required for viper to merge correctly) and set the defaults (will be used if not explicitly set via environment or config file)
-	fmt.Printf("\n\nDefault CONFIG IS %+v\n\n", defaultConfig)
 	viper.SetDefault("spec", config.GetMap(defaultConfig.AppSpec))
 
 	err := viper.ReadInConfig()
@@ -81,35 +70,19 @@ func loadConfig(srcFile string, fl flag.FlagSet) (*Config, error) {
 	if err := viper.Unmarshal(&config); err != nil {
 		return nil, err
 	}
-
-
-
-
-	fmt.Printf("\nkeys : %+v", viper.AllKeys())
-
 	return &config, nil
 }
 
 func (c Config) GetMap(config interface{}) map[string]interface{} {
-
 	var inInterface map[string]interface{}
 	mapstructure.Decode(config, &inInterface)
-
-	fmt.Printf("inInterface is %+v", inInterface)
-
 	return inInterface
 }
 
 func main() {
 	flag.Parse()
-
-	x, _ := ioutil.ReadFile(*configFile)
-	fmt.Println(string(x))
-
-
-	fmt.Printf("\n\nFlagConfig IS %+v\n\n", flagConfig)
-
 	config, _ := loadConfig(*configFile, *flag.CommandLine)
+	fmt.Printf("\n\nDefault CONFIG IS %+v\n\n", defaultConfig)
 	fmt.Printf("\n\nFLAGS are \n\t%+v\n\t%+v\n\t%+v\n", *stringConfig, *intConfig, *boolConfig)
-	fmt.Printf("\n\nCONFIG IS %+v\n\n", config)
+	fmt.Printf("\n\nCONFIG IS \n%+v\n\n", config)
 }
