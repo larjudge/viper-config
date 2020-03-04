736b5f04bf45efe80b29fd88bdf634b3a5e8f88c
Lar Judge <larjudge@gmail.com>
2020-03-04 17:36:16 +1100

    Feature/yaml (#1)
    * working with flags and yaml

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-`go run main.go --intConfig=1 --stringConfig="hello world" --boolConfig=true`
+# Viper-Config
+
+Viper-Config is a really simple demo application that uses viper to parse both config files and command line flags
+
+Running flags: 
+```zsh
+go run main.go --spec.intConfig=1 --spec.stringConfig="hello world" --spec.boolConfig=true
+```
+
+Running with config file:
+```zsh
+go run main.go --config "$PWD/config/local.yaml"
+```
+
+Running with both:
+```zsh
+go run main.go --config "$PWD/config/local.yaml" --spec.stringConfig="yaml and flags"
+```
--- main.go
+++ main.go
@@ -2,51 +2,87 @@ package main
 
 import (
 	"fmt"
+	"github.com/mitchellh/mapstructure"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"log"
-)
-
-const (
-	boolValue   = false
-	stringValue = "some string"
-	intValue    = 1234
 )
 
 var (
-	boolConfig   bool
-	intConfig    int
-	stringConfig string
+	configFile = flag.StringP("config", "c", "", "specific config file to use")
+	boolConfig   = flag.BoolP("spec.boolConfig", "b", false, "some bool")
+	intConfig    = flag.IntP("spec.intConfig", "i", 0, "some int")
+	stringConfig = flag.StringP("spec.stringConfig", "s", "DEFAULT", "some string")
 )
 
-type AppConfig struct {
-	BoolConfig   bool   `yaml:"boolConfig"`
-	IntConfig    int    `yaml:"intConfig"`
-	StringConfig string `yaml:"stringConfig"`
+type Config struct {
+	AppSpec AppSpec `json:"spec" yaml:"spec" mapstructure:"spec"`
 }
 
-func LoadConfig() (*AppConfig, error) {
-	flag.IntVarP(&intConfig, "intConfig", "i", intValue, "some int")
-	flag.StringVarP(&stringConfig, "stringConfig", "s", stringValue, "some string")
-	flag.BoolVarP(&boolConfig, "boolConfig", "b", boolValue, "some bool")
+type AppSpec struct {
+	BoolConfig   bool   `json:"boolConfig" yaml:"boolConfig" mapstructure:"boolConfig"`
+	IntConfig    int    `json:"intConfig" yaml:"intConfig" mapstructure:"intConfig"`
+	StringConfig string `json:"stringConfig" yaml:"stringConfig" mapstructure:"stringConfig"`
+}
 
-	flag.Parse()
+var (
+	defaultConfig = &Config{
+		AppSpec: AppSpec{
+			BoolConfig:   *boolConfig,
+			IntConfig:    *intConfig,
+			StringConfig: *stringConfig,
+		},
+	}
+)
 
-	err := viper.BindPFlags(flag.CommandLine)
+func loadConfig(srcFile string, fl flag.FlagSet) (*Config, error) {
+	viper.BindPFlags(&fl)
+
+	if srcFile != "" {
+		viper.SetConfigFile(srcFile)
+	} else {
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+		viper.AddConfigPath("/config")
+		viper.AddConfigPath("/app/config")
+	}
+
+	var config Config
+
+	// convert from struct to generic map (required for viper to merge correctly) and set the defaults (will be used if not explicitly set via environment or config file)
+	viper.SetDefault("spec", config.GetMap(defaultConfig.AppSpec))
+
+	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatal("cannot bind command line flags")
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println(viper.ConfigFileUsed())
+			if err := viper.Unmarshal(&config); err != nil {
+				fmt.Println(fmt.Errorf("Fatal unmarshal config file: %s \n", err))
+				return nil, err
+			}
+			return &config, nil
+		} else {
+			// if no config file found use the default configuration
+			fmt.Println("error:")
+			fmt.Println(err)
+			return defaultConfig, err
+		}
 	}
-
-	config := &AppConfig{
-		BoolConfig:   boolConfig,
-		IntConfig:    intConfig,
-		StringConfig: stringConfig,
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
 	}
-
-	return config, nil
+	return &config, nil
 }
 
-func main () {
-	config, _ := LoadConfig()
-	fmt.Printf("\n\nCONFIG IS %+v\n\n", config)
+func (c Config) GetMap(config interface{}) map[string]interface{} {
+	var inInterface map[string]interface{}
+	mapstructure.Decode(config, &inInterface)
+	return inInterface
+}
+
+func main() {
+	flag.Parse()
+	config, _ := loadConfig(*configFile, *flag.CommandLine)
+	fmt.Printf("Default CONFIG IS \n\t%+v\n\n", defaultConfig)
+	fmt.Printf("\n\nFLAGS are \n\t%+v\n\t%+v\n\t%+v\n", *stringConfig, *intConfig, *boolConfig)
+	fmt.Printf("\n\nCONFIG IS \n\t%+v\n\n", config)
 }
